9477bb17ae172b06a8990f2843108cd763201f16
IgnacioDiazV <nachodiazvaldez@gmail.com>
2025-06-07 15:41:05 -0300

    variables

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 // Nombre: Díaz Valdez, Ignacio - Legajo: 61551
-import Home from "./components/Home";
+import HomePages from "./pages/HomePages";
 
 function App() {
+  // Datos a pasar
+  let nombre = "Pablo";
+
+  let persona = {
+    nombre: 'Carlos',
+    edad: 20,
+    curso: 'programación',
+    Asistencia: false
+  };
+
+  let grupo = [
+    { nombre: 'Lucas', edad: 25 },
+    { nombre: 'Luciana', edad: 19 },
+    { nombre: 'Julieta', edad: 26 },
+    { nombre: 'Jorge', edad: 30 },
+    { nombre: 'Cristian', edad: 30 }
+  ];
 
   return (
     <div className="App">
-      
+      {/* Pasa todo a HomePages */}
+      <HomePages nombre={nombre} persona={persona} grupo={grupo} />
     </div>
   );
-  
 }
 
 export default App;
